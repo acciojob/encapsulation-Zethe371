07ff04feec542c574cf0fbde1be0128cf94e333e
farhan.2763 <farhan2763@gmail.com>
2023-10-07 01:39:24 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
-  
+    public static void main(String args[])
+    {
+        RWOnly obj= new RWOnly();
+
+        //name is private variable in class RWOnly, hence cannot access it
+//      System.out.println(obj.name);
+//      //Error: java: name has private access in com.driver.RWOnly
+        obj.setName("Adil Masood");
+        System.out.println(obj.getName());
+    }
 }
